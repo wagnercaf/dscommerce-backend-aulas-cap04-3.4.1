bede5483874202b9c032f69d8865550e68a1207b
wagnercaf <wagnercaf@hotmail.com>
2025-02-10 22:39:50 -0300

    Add os getters e settes no UserDTO

--- src/main/java/com/devsuperior/dscommerce/dto/UserDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/UserDTO.java
@@ -17,6 +17,16 @@ public class UserDTO {
 
     private List<String> roles = new ArrayList<>();
 
+    public UserDTO(Long id, String name, String email, String phone, LocalDate birthDate, String password, List<String> roles) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.password = password;
+        this.roles = roles;
+    }
+
     public UserDTO(User entity) {
         id = entity.getId();
         name = entity.getName();
@@ -27,5 +37,61 @@ public class UserDTO {
         for (GrantedAuthority role : entity.getRoles()) {
             roles.add(role.getAuthority());
         }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
     }
 }
